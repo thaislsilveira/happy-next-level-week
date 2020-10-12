fef59f65d28826c4ea8f256d1130f2d8438cd982
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-12 14:41:01 -0300

    criando rotas

---
+++ web/src/routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Lading from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Lading} exact />
+        <Route path="/app" component={OrphanagesMap} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
