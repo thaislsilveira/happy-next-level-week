19d6b34c3a57dc90840f0af405b40b35b7c33ac3
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-10-17 15:32:58 -0300

    moficiações web

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -79,7 +79,7 @@ export default function CreateOrphanage() {
             <legend>Dados</legend>
 
             <Map
-              center={[-27.2092052, -49.6401092]}
+              center={[-20.2836333, -50.5440513]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
               onClick={handleMapClick}
